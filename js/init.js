7a9f0729f98c6cd1cda5e50bcf7f898dd701ce96
Ryan O'Connor <rocsteady888@gmail.com>
2018-05-28 21:53:57 -0400

    changed parallax format

--- js/init.js
+++ js/init.js
@@ -1,7 +1,8 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
